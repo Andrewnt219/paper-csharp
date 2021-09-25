5fa9b4e8cb9392864a7cfa9af6ba69b3b320c7c5
Andrew <phongnguyentuan20@gmail.com>
2021-09-25 13:36:21 -0400

    added method CreateDistDir in Generator

--- modules/cli/Generator.cs
+++ modules/cli/Generator.cs
@@ -1,6 +1,11 @@
 
 
+using System.Text;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using paper_csharp.modules.file_parser;
 namespace paper_csharp.modules.cli
 {
 
@@ -15,10 +20,91 @@ namespace paper_csharp.modules.cli
 
     public void Run()
     {
+      this.CreateDistDir();
+      this.GenerateDistFiles();
+    }
 
-      Console.WriteLine($"{Args.DistDirPath} {Args.InputPaths} {Args.StylesheetUrl}");
+    private void CreateDistDir()
+    {
+      if (Directory.Exists(Args.DistDirPath))
+      {
+        Directory.Delete(Args.DistDirPath, true);
+      }
+
+      Directory.CreateDirectory(Args.DistDirPath);
+    }
+
+    private void GenerateDistFiles()
+    {
+      foreach (string path in Args.InputPaths)
+      {
+        this.GenerateDistFromPath(path);
+      }
 
     }
 
+    private void GenerateDistFromPath(string path)
+    {
+      if (Directory.Exists(path))
+      {
+        this.GenerateDistFromDir(path);
+        return;
+      }
+
+      if (File.Exists(path))
+      {
+        this.GenerateDistFromFile(path);
+        return;
+      }
+
+      System.Console.WriteLine($"Path is not recognized: {path}");
+      System.Environment.Exit(1);
+      return;
+    }
+
+    private void GenerateDistFromDir(string dirPath)
+    {
+      if (!Directory.Exists(dirPath))
+      {
+        return;
+      }
+
+
+      Directory.CreateDirectory(Path.Join(Args.DistDirPath, dirPath));
+
+      string[] subpaths = Directory.GetFileSystemEntries(dirPath);
+      foreach (string path in subpaths)
+      {
+        this.GenerateDistFromPath(path);
+      }
+    }
+
+    private void GenerateDistFromFile(string filePath)
+    {
+      if (!File.Exists(filePath))
+      {
+        return;
+      }
+
+      try
+      {
+        List<string> lines = File.ReadAllLines(filePath).ToList();
+        ParseResult result = TextFile.Parse(lines);
+        string content = HtmlFile.Parse(result);
+
+        var distPath = Path.Join(Args.DistDirPath, Path.GetDirectoryName(filePath), $"{Path.GetFileNameWithoutExtension(filePath)}.html");
+        var htmlFile = File.Create(distPath);
+        htmlFile.Write(Encoding.ASCII.GetBytes(content));
+        htmlFile.Close();
+      }
+      catch (Exception ex)
+      {
+        System.Console.WriteLine($"Fail to generate dist files: {ex.Message}");
+        System.Environment.Exit(1);
+      }
+
+    }
   }
+
+
 }
