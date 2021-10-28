99ad73026c60f23cd566f45f1ecdd5befbf398e0
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2021-10-28 00:44:49 -0400

    feat: support static images (#8)
    * parse markdown img
    
      * add class MarkdownParser
    
      * parse markdown img
    
    * copy static files to dist
    
      * add CreateDirForce
    
      * add DirectoryCopy
    
      * add CreateDirSoft
    
      * copy static files
    
    * rewrite staitc files src
    
    * show full path in index file
    
    * update build

---
+++ modules/utils/DirectoryUtils.cs
@@ -0,0 +1,70 @@
+using System.IO;
+
+
+namespace paper_csharp.modules.utils
+{
+  public class DirectoryUtils
+  {
+
+    // Overwrite or create a new directory at path
+    public static DirectoryInfo CreateDirForce(string path)
+    {
+      if (Directory.Exists(path))
+      {
+        Directory.Delete(path, true);
+      }
+
+      return Directory.CreateDirectory(path);
+    }
+
+    // Create a new directory if not exist at path
+    public static DirectoryInfo CreateDirSoft(string path)
+    {
+      DirectoryInfo dir = new DirectoryInfo(path);
+
+      if (dir.Exists)
+      {
+        return dir;
+      }
+
+      return Directory.CreateDirectory(dir.ToString());
+    }
+
+    public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+    {
+      // Get the subdirectories for the specified directory.
+      DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+      if (!dir.Exists)
+      {
+        throw new DirectoryNotFoundException(
+            "Source directory does not exist or could not be found: "
+            + sourceDirName);
+      }
+
+      DirectoryInfo[] dirs = dir.GetDirectories();
+
+      // If the destination directory doesn't exist, create it.       
+      Directory.CreateDirectory(destDirName);
+
+      // Get the files in the directory and copy them to the new location.
+      FileInfo[] files = dir.GetFiles();
+      foreach (FileInfo file in files)
+      {
+        string tempPath = Path.Combine(destDirName, file.Name);
+        file.CopyTo(tempPath, false);
+      }
+
+      // If copying subdirectories, copy them and their contents to new location.
+      if (copySubDirs)
+      {
+        foreach (DirectoryInfo subdir in dirs)
+        {
+          string tempPath = Path.Combine(destDirName, subdir.Name);
+          DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+        }
+      }
+    }
+  }
+
+}
