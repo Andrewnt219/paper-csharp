dbf995d6fed6c1e8f463de1207aed290c3861d36
Andrew Nguyen <phongnguyentuan20@gmail.com>
2021-10-28 10:27:32 -0400

    support full markdown

--- modules/file_parser/MarkdownFile.cs
+++ modules/file_parser/MarkdownFile.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Text.RegularExpressions;
-using System.Linq;
+using Markdig;
 using System.IO;
-using System.Collections.Generic;
 
 namespace paper_csharp.modules.file_parser
 {
@@ -18,96 +16,13 @@ namespace paper_csharp.modules.file_parser
     static public ParseResult Parse(string filePath)
     {
       string fileContent = File.ReadAllText(filePath);
+      string staticAssetPatter = @"!\[(.*)\]\((.*)\)";
 
       string title = Path.GetFileNameWithoutExtension(filePath);
-      string body = "";
-      // Separates elements by double newline
-      IEnumerable<string> elements = Regex.Split(fileContent, "(\r\r|\n\n|\r\n\r\n)").Where(str => !string.IsNullOrWhiteSpace(str));
-
-      foreach (string element in elements)
-      {
-        body += MarkdownFile.ParseElement(element);
-      }
+      // Rewrite static asssets
+      string body = Markdown.ToHtml(Regex.Replace(fileContent, staticAssetPatter, "![$1](/$2)"));
 
       return new ParseResult(title, body);
-    }
-
-    /// <summary>
-    ///   Parse a markdown element (line) to html
-    /// </summary>
-    private static string ParseElement(string element)
-    {
-      MarkdownParser parser = new MarkdownParser(element);
-
-      parser.ReplaceSingleNewline().ParseBoldText().ParseHorizontalLine().ParseImage();
-
-      return $"<p>{parser.Element}</p>";
-    }
-
-
-  }
-
-  public class MarkdownParser
-  {
-    public string Element;
-
-    public MarkdownParser(string element)
-    {
-      Element = element;
-    }
-
-    /// <summary>
-    ///   Replace single newlines with space
-    /// </summary>
-    public MarkdownParser ReplaceSingleNewline()
-    {
-      Element = Regex.Replace(Element, "(\r|\n|\r\n)", " ");
-
-      return this;
-    }
-
-    /// <summary>
-    ///   Parse markdown bold to tag `strong`
-    /// </summary>
-    public MarkdownParser ParseBoldText()
-    {
-      // Match anything between two double-asterisk or two double-lodash
-      string boldPattern = @"\*{2}(.*?)\*{2}|_{2}(.*?)_{2}";
-      string boldReplacement = "<strong>$1</strong>";
-
-      Element = Regex.Replace(Element, boldPattern, boldReplacement);
-
-      return this;
-    }
-
-    /// <summary>
-    ///   Parse markdown --- to tag `hr`
-    /// </summary>
-    public MarkdownParser ParseHorizontalLine()
-    {
-      // Match if --- is the only chars in the line
-      string hrPattern = @"^---$";
-      string hrReplacement = "<hr />";
-
-      Element = Regex.Replace(Element, hrPattern, hrReplacement);
-
-      return this;
-    }
-
-    /// <summary>
-    ///   Parse markdown [alt](src) to tag `img`
-    /// </summary>
-    public MarkdownParser ParseImage()
-    {
-      string imgPattern = @"\[(?<alt>.*)\]\((?<src>.*)\)";
-      var match = Regex.Match(Element, imgPattern);
-
-      if (match.Success)
-      {
-        Element = $"<img style=\"width:100%\" alt=\"{match.Groups["alt"]}\" src=\"/static/{match.Groups["src"]}\" />";
-      }
-
-      return this;
     }
   }
 }
